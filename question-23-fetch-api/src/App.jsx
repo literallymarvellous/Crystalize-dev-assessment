ecba2abdd8591c7eca32d894f4be2a19d275d14c
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-05-16 16:15:45 +0100

    question 22

--- question-23-fetch-api/src/App.jsx
+++ question-23-fetch-api/src/App.jsx
@@ -1,45 +1,98 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tokenInput, setTokenInput] = useState("");
+  // const [query, setQuery] = useState("");
+  const [tokenList, setTokenList] = useState([]);
+  const [tokenInfo, setTokenInfo] = useState();
+  // const [isError, setIsError] = useState("");
+  const isMounted = useRef(0);
+
+  const fetchToken = async () => {
+    try {
+      setTokenInfo(null);
+      let res = await fetch(
+        `https://api.coingecko.com/api/v3/search?query=${tokenInput}`
+      );
+      let data = await res.json();
+      console.log(data.coins);
+      setTokenList(data.coins);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchPrice = async (token) => {
+    try {
+      let res = await fetch(`https://api.coingecko.com/api/v3/coins/${token}`);
+      let data = await res.json();
+      console.log(data);
+      setTokenInfo(data);
+      setTokenList([]);
+      setTokenInput("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    if (isMounted.current >= 2 && tokenInput) {
+      fetchToken();
+    } else {
+      isMounted.current += 1;
+    }
+  }, [tokenInput]);
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setQuery(tokenInput);
+  //   setTokenInput("");
+  //   setPriceInfo("");
+  // };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div>Search and get token prices</div>
+
+      <form>
+        <input
+          type="text"
+          value={tokenInput}
+          onChange={(e) => setTokenInput(e.target.value)}
+        />
+      </form>
+
+      <ul>
+        {tokenList.length > 0
+          ? tokenList.slice(0, 5).map((token) => (
+              <li key={token.name}>
+                <div onClick={() => fetchPrice(token.id)}>
+                  {" "}
+                  <img src={token.thumb} /> {token.name} ({token.symbol})
+                </div>
+              </li>
+            ))
+          : null}
+      </ul>
+
+      <div>
+        {tokenInfo && (
+          <div>
+            <div>
+              <img
+                src={tokenInfo.image.thumb}
+                alt={`${tokenInfo.name} image`}
+              />{" "}
+              {tokenInfo.name} ({tokenInfo.symbol})
+            </div>
+            <div>Current Price: ${tokenInfo.market_data.current_price.usd}</div>
+            <div>Market Cap: ${tokenInfo.market_data.market_cap.usd}</div>
+            <div>All-Time High: ${tokenInfo.market_data.ath.usd}</div>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
